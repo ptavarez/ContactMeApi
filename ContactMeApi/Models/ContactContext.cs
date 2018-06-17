593aebade901de9350d456d123a0129949346e65
ptavarez16 <ptavarez16@yahoo.com>
2018-06-17 19:20:07 -0400

    Create database context - The database context is the main class that coordinates Entity Framework functionality for a given data model. - Create class by deriving from the Microsoft.EntityFrameworkCore.DbContext class - Add ContactContext class to Models folder

---
+++ ContactMeApi/Models/ContactContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ContactMeApi.Models
+{
+    public class ContactContext : DbContext
+    {
+        public ContactContext(DbContextOptions<ContactContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
