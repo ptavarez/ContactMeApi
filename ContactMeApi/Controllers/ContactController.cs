3e8d535ba3edf7ea3e9f68b929de8e75cb153355
ptavarez16 <ptavarez16@yahoo.com>
2018-06-17 19:36:52 -0400

    Add Contact Controller - Replace generated code with appropriate code that:   - Defines an API controller class without methods   - Used Dependency injection to inject database context into the   Controller   - Database context is used to in each CRUD methods in the Controller   - Constructor adds an item to the in-memory database if one doesn't   exist

---
+++ ContactMeApi/Controllers/ContactController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using TodoApi.Models;
+
+namespace ContactMeApi.Controllers
+{
+    public class ContactController
+    {
+        public ContactController()
+        {
+        }
+    }
+}
