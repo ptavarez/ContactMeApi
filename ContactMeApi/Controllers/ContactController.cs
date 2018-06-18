54802475d21acaa4bb4ed8de215dc4d2acdbb53e
ptavarez16 <ptavarez16@yahoo.com>
2018-06-17 20:02:19 -0400

    Get contacts - Add method to get all contacts - Add method to get single contact

--- ContactMeApi/Controllers/ContactController.cs
+++ ContactMeApi/Controllers/ContactController.cs
@@ -1,14 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
-using TodoApi.Models;
+using ContactMeApi.Models;
 
 namespace ContactMeApi.Controllers
 {
-    public class ContactController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
     {
-        public ContactController()
+        private readonly ContactContext _context;
+
+        public ContactController(ContactContext context)
         {
+            _context = context;
+
+            if (_context.Contacts.Count() == 0)
+            {
+                _context.Contact.Add(new Contact 
+                { 
+                    FirstName = "Dro", 
+                    LastName = "Tavarez",
+                    JobTitle = "Software Developer",
+                    PhoneNumber = "978-239-9784",
+                    Email = "pe@dro.com"
+                });
+                _context.SaveChanges();
+            }
+        }
+        [HttpGet]
+        public ActionResult<List<Contact>> GetAll()
+        {
+            return _context.Contacts.ToList();
+        }
+        [HttpGet("{id}", Name = "GetContact")]
+        public ActionResult<Contact> GetById(long id)
+        {
+            var item = _context.Contacts.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return item;
         }
     }
 }
