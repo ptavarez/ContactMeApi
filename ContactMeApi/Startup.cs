4478521deee791ee7cff738eef24175f62972559
ptavarez16 <ptavarez16@yahoo.com>
2018-06-18 13:48:06 -0400

    Add auth0 authentication service - Replace original authentication service

--- ContactMeApi/Startup.cs
+++ ContactMeApi/Startup.cs
@@ -28,20 +28,17 @@ namespace ContactMeApi
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-              .AddJwtBearer(options =>
-              {
-                  options.TokenValidationParameters = new TokenValidationParameters
-                  {
-                      ValidateIssuer = true,
-                      ValidateAudience = true,
-                      ValidateLifetime = true,
-                      ValidateIssuerSigningKey = true,
-                      ValidIssuer = Configuration["Jwt:Issuer"],
-                      ValidAudience = Configuration["Jwt:Issuer"],
-                      IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
-                  };
-              });  
+            string domain = $"https://{Configuration["Auth0:Domain"]}/";
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.Authority = domain;
+                options.Audience = Configuration["Auth0:Audience"];
+            }); 
 
             services.AddDbContext<ContactContext>(opt =>
                 opt.UseInMemoryDatabase("Contact"));
