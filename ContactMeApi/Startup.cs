681b7e38ca330f24152455f748a6c81d4b8601d1
ptavarez16 <ptavarez16@yahoo.com>
2018-06-18 14:31:25 -0400

    500 error bug patch - Was running into an error when attempting to retrieve contacts - Fix: Remove domain variable and set options.Authority to "Configuration["Auth0:Authority"];"

--- ContactMeApi/Startup.cs
+++ ContactMeApi/Startup.cs
@@ -28,15 +28,13 @@ namespace ContactMeApi
         public void ConfigureServices(IServiceCollection services)
         {
 
-            string domain = $"https://{Configuration["Auth0:Domain"]}/";
-
             services.AddAuthentication(options =>
             {
                 options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
                 options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
             }).AddJwtBearer(options =>
             {
-                options.Authority = domain;
+                options.Authority = Configuration["Auth0:Authority"];
                 options.Audience = Configuration["Auth0:Audience"];
             }); 
 
